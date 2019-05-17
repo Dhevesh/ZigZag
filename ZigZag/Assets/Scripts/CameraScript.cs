a4bf2b1b8cdcfd17003b4010278a152ec4255246
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-17 14:39:37 +0100

    created player controller and camera controller.

---
+++ ZigZag/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    void Awake()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate() //used for camera updates
+    {
+        transform.position = target.position + offset;
+    }
+}
