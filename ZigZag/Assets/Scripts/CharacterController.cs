28077f82a168e6a2c3d560cfff857a06bc8c389c
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-17 17:11:32 +0100

    added functionality to increase score and display on screen.

--- ZigZag/Assets/Scripts/CharacterController.cs
+++ ZigZag/Assets/Scripts/CharacterController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterController : MonoBehaviour
 {
@@ -8,43 +9,78 @@ public class CharacterController : MonoBehaviour
     private bool walkingRight = true;
     public Transform rayStart;
 
+    private GameManager gameManager;
+
     private Animator anim;
+
+    public Text playerScore;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     private void FixedUpdate()
     {
-        rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
+        if (!gameManager.gameStarted)
+        {
+            return;
+        }
+        else
+        {
+            anim.SetTrigger("isIdle");
+            rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-        {
+        {   
             Switch();
         }
+        
         RaycastHit hit;
         if (!Physics.Raycast(rayStart.position,-transform.up,out hit, Mathf.Infinity))
         {
             anim.SetTrigger("isFalling");
         }
+        if (rb.transform.position.y < -2)
+        {
+            gameManager.EndGame();
+        }
     }
 
     private void Switch()
     {
+        if (!gameManager.gameStarted)
+        {
+            return;
+        }
         walkingRight = !walkingRight;
 
         if (walkingRight)
         {
             transform.rotation = Quaternion.Euler(0, 45, 0);
+            
         }
         else
         {
             transform.rotation = Quaternion.Euler(0, -45, 0);
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Crystal")
+        {
+            Destroy(collider.gameObject);
+            gameManager.IncreaseScore();
+            playerScore.text = gameManager.score.ToString();
         }
     }
 }
---
+++ ZigZag/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameStarted;
+    public int score;
+    
+
+    public void StartGame()
+    {
+        gameStarted = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartGame();
+        }
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+    }
+}
